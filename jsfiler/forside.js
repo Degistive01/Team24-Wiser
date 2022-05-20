97217a7e643c587514fe25ca5ed0c38d250f4d22
tjmaucl <tjma38750@edu.ucl.dk>
2022-05-20 20:52:39 +0200

    js loop forside

--- jsfiler/forside.js
+++ jsfiler/forside.js
@@ -1,1 +1,10 @@
-console.console.log(js is running);
+console.log("js to forside is running");
+
+const insigterfunktion = ["- Glemsomhed", "- Kage", "- Overmod"];
+
+let text = "";
+for (let i = 0; i < insigterfunktion.length; i++) {
+  text += insigterfunktion[i] + "<br>";
+}
+
+document.getElementById("indsigter").innerHTML = text;
