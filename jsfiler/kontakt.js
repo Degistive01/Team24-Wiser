4de9b0a92a6f24ec0c013e6998205d14676f6df0
Degistive01 <mateuszjanslup@gmail.com>
2022-05-22 18:13:26 +0200

    js kontakt

---
+++ jsfiler/kontakt.js
@@ -0,0 +1,13 @@
+const nameInput = document.querySelector("#name");
+const email = document.querySelector("#email");
+const message = document.querySelector("#message");
+const succes = document.querySelector("#success");
+const errorNodes = document.querySelectorAll(".error");
+
+
+
+
+function validateForm() {
+    console.log("succes");
+
+}
