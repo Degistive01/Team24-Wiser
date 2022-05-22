0981e0736d2f2ef72f261008c36c651d32d09da2
JETEXODUS <95678727+JETEXODUS@users.noreply.github.com>
2022-05-22 18:19:03 +0200

    ændret lidt

--- jsfiler/kontakt.js
+++ jsfiler/kontakt.js
@@ -1,7 +1,7 @@
 const nameInput = document.querySelector("#name");
 const email = document.querySelector("#email");
 const message = document.querySelector("#message");
-const succes = document.querySelector("#success");
+const succes = document.querySelector("#succes");
 const errorNodes = document.querySelectorAll(".error");
 
 
