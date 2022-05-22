5eaae0a69a4f511a1dedc2f226e890e2e59c336c
Degistive01 <mateuszjanslup@gmail.com>
2022-05-22 19:43:00 +0200

    Fix til JS

--- jsfiler/kontakt.js
+++ jsfiler/kontakt.js
@@ -2,13 +2,12 @@ const nameInput = document.querySelector("#name");
 const email = document.querySelector("#email");
 const message = document.querySelector("#message");
 const success = document.querySelector("#success");
-const errorNodes = document.querySelector(".error");
+const errorNodes = document.querySelectorAll(".error");
 
 
 function validateForm() {
-
-    if (nameInput.value.lenght < 1) {
-        errorNodes[0].innerText = "Name cannot be blank";
+    if (nameInput.value.length < 1) {
+        errorNodes[0].innerText = "Udfylt Navn";
         nameInput.classList.add("error-border");
     }
 }
