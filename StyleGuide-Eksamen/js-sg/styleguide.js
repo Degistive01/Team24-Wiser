57f97cbccf621b89d0dd945226e7cdb40c40cdba
JETEXODUS <95678727+JETEXODUS@users.noreply.github.com>
2022-05-27 16:20:48 +0200

    oprettet rigtig javascript

---
+++ StyleGuide-Eksamen/js-sg/styleguide.js
@@ -0,0 +1,15 @@
+console.log("hey jeg virker");
+
+let hsd = [];
+hsd [0] = "SUPER!";
+hsd [1] = "ØV...:(";
+
+function myFunction() {
+let valg = document.getElementById("areyouready");
+if (valg == 1) {
+    alert(hsd [0]);
+  }
+  else (valg == 2){
+    alert(hsd [1])
+  }
+}
